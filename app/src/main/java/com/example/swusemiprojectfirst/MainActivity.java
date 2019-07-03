ee012edae341621a9964d7d7aac996aef8124c62
wineok1234 <wieok@naver.com>
2019-07-03 14:55:08 +0900

    첫번째 나의 커밋이다.

--- app/src/main/java/com/example/swusemiprojectfirst/MainActivity.java
+++ app/src/main/java/com/example/swusemiprojectfirst/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //테스트 코드1
+
+        // 모듈 커디션 중간
+
+        // 커밋 및 push 테스트
+
     }
 }
