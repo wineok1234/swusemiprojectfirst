884198202fd2bbec425f2f1d9e572fb73fbc7675
wineok1234 <wieok@naver.com>
2019-07-03 16:50:26 +0900

    메인과 회원가입의 xml을 작성함

--- app/src/main/java/com/example/swusemiprojectfirst/MainActivity.java
+++ app/src/main/java/com/example/swusemiprojectfirst/MainActivity.java
@@ -11,11 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //테스트 코드1
-
-        // 모듈 커디션 중간
-
-        // 커밋 및 push 테스트
-
     }
 }
